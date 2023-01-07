88476b9cb9c4b6c0160a5cbc62a546d2402391bf
StephenHosking <stephen@rpnscientific.com>
2023-01-08 09:44:01 +1030

    Initial commit

---
+++ daily-ctd.ps1
@@ -0,0 +1,14 @@
+cd E:\Drives\Dev\NoPowerTxt\
+cd .\M2mMonitor\
+
+$pathStr = "./Reports/DailyReports/"
+$errorOutputPrefix = $pathStr + "device_writer_errors-dailyctd-" 
+$date_str=(Get-Date).ToString("yyyyMMdd")
+$errorOutputFile = $errorOutputPrefix + $date_str + ".txt" 
+$scriptOutputFile = $pathStr + "daily-ctd-output.txt"
+
+$null | sc $errorOutputFile
+rm $scriptOutputFile
+
+.\publish\latest\net5.0\M2mMonitor.exe putDbUsagesCtd --putDb --errorOutput=$errorOutputFile 1>$scriptOutputFile
+

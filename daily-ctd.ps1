2a59e8e39482943a2c55112f88998d0d814befe4
StephenHosking <stephen@rpnscientific.com>
2023-01-16 08:42:36 +1030

    date suffix on both output files - as run

--- daily-ctd.ps1
+++ daily-ctd.ps1
@@ -1,11 +1,12 @@
-cd E:\Drives\Dev\NoPowerTxt\
+pushd E:\Drives\Dev\NoPowerTxt\
 cd .\M2mMonitor\
 
 $pathStr = "./Reports/DailyReports/"
-$errorOutputPrefix = $pathStr + "device_writer_errors-dailyctd-" 
-$date_str=(Get-Date).ToString("yyyyMMdd")
-$errorOutputFile = $errorOutputPrefix + $date_str + ".txt" 
-$scriptOutputFile = $pathStr + "daily-ctd-output.txt"
+$errorOutputPrefix = $pathStr + "device_writer_errors" 
+$dateStr=(Get-Date).ToString("yyyyMMdd")
+$dateSuffix = "." + $dateStr + ".txt"
+$errorOutputFile = $errorOutputPrefix + $dateSuffix 
+$scriptOutputFile = $pathStr + "daily-ctd-output" + $dateSuffix
 
 $null | sc $errorOutputFile
 rm $scriptOutputFile
@@ -13,6 +14,10 @@ rm $scriptOutputFile
 # Don't know if the skipCtdInsert is needed for the Deactivated and Retired devices. 
 # Don't want hundreds of 0's for these, but not confirmed there's a problem
 
-.\baselines\latest\net5.0\M2mMonitor.exe putDbUsagesCtd --simStatus=Activated   --putDb --NskipCtdInsert --NshowSql --errorOutput=$errorOutputFile 1>$scriptOutputFile
-.\baselines\latest\net5.0\M2mMonitor.exe putDbUsagesCtd --simStatus=Deactivated --putDb --skipCtdInsert --NshowSql --errorOutput=$errorOutputFile 1>>$scriptOutputFile
-.\baselines\latest\net5.0\M2mMonitor.exe putDbUsagesCtd --simStatus=Retired     --putDb --skipCtdInsert --NshowSql --errorOutput=$errorOutputFile 1>>$scriptOutputFile
+Set-Alias -Name mon -Value .\baselines\latest\net5.0\M2mMonitor.exe
+
+mon putDbUsagesCtd --simStatus=Activated   --NputDb --NskipCtdInsert  --NshowSql --deviceLimit=5 --errorOutput=$errorOutputFile 1>$scriptOutputFile
+mon putDbUsagesCtd --simStatus=Deactivated --NputDb --skipCtdInsert   --NshowSql --deviceLimit=5 --errorOutput=$errorOutputFile 1>>$scriptOutputFile
+mon putDbUsagesCtd --simStatus=Retired     --NputDb --skipCtdInsert   --NshowSql --deviceLimit=5 --errorOutput=$errorOutputFile 1>>$scriptOutputFile
+
+popd

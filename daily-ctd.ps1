a353a752d63ce445e360feab4986dffc6e16e3bf
StephenHosking <stephen@rpnscientific.com>
2023-01-15 14:48:17 +1030

    Daily run to include Deactivated and Retired devices! Woot!

--- daily-ctd.ps1
+++ daily-ctd.ps1
@@ -10,5 +10,9 @@ $scriptOutputFile = $pathStr + "daily-ctd-output.txt"
 $null | sc $errorOutputFile
 rm $scriptOutputFile
 
-.\baselines\latest\net5.0\M2mMonitor.exe putDbUsagesCtd --putDb --errorOutput=$errorOutputFile 1>$scriptOutputFile
+# Don't know if the skipCtdInsert is needed for the Deactivated and Retired devices. 
+# Don't want hundreds of 0's for these, but not confirmed there's a problem
 
+.\baselines\latest\net5.0\M2mMonitor.exe putDbUsagesCtd --simStatus=Activated   --putDb --NskipCtdInsert --NshowSql --errorOutput=$errorOutputFile 1>$scriptOutputFile
+.\baselines\latest\net5.0\M2mMonitor.exe putDbUsagesCtd --simStatus=Deactivated --putDb --skipCtdInsert --NshowSql --errorOutput=$errorOutputFile 1>>$scriptOutputFile
+.\baselines\latest\net5.0\M2mMonitor.exe putDbUsagesCtd --simStatus=Retired     --putDb --skipCtdInsert --NshowSql --errorOutput=$errorOutputFile 1>>$scriptOutputFile

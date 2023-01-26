36e7f0928e702a9c770ab62bd42d0c47c7c53b08
StephenHosking <stephen@rpnscientific.com>
2023-01-27 07:01:04 +1030

    As done, for daily. Added WIP, with no write to DB and to show SQL

--- daily-ctd.ps1
+++ daily-ctd.ps1
@@ -9,6 +9,7 @@ $errorOutputFile = $errorOutputPrefix + $dateSuffix
 $scriptOutputFile = $pathStr + "daily-ctd-output" + $dateSuffix
 
 $null | sc $errorOutputFile
+
 rm $scriptOutputFile
 
 # Don't know if the skipCtdInsert is needed for the Deactivated and Retired devices. 
@@ -16,8 +17,32 @@ rm $scriptOutputFile
 
 Set-Alias -Name mon -Value .\baselines\latest\net5.0\M2mMonitor.exe
 
-mon putDbUsagesCtd --simStatus=Activated   --putDb --NskipCtdInsert  --NshowSql --errorOutput=$errorOutputFile 1>$scriptOutputFile
-mon putDbUsagesCtd --simStatus=Deactivated --putDb --skipCtdInsert   --NshowSql --errorOutput=$errorOutputFile 1>>$scriptOutputFile
-mon putDbUsagesCtd --simStatus=Retired     --putDb --skipCtdInsert   --NshowSql --errorOutput=$errorOutputFile 1>>$scriptOutputFile
+mon putDbUsagesCtd --simStatus=Activated   --putDb --NskipCtdInsert  --errorOutput=$errorOutputFile 1>$scriptOutputFile
+mon putDbUsagesCtd --simStatus=Deactivated --putDb --skipCtdInsert   --errorOutput=$errorOutputFile 1>>$scriptOutputFile
+mon putDbUsagesCtd --simStatus=Retired     --putDb --skipCtdInsert   --errorOutput=$errorOutputFile 1>>$scriptOutputFile
+
+popd
+
+pushd E:\Drives\Dev\NoPowerTxt\
+cd .\M2mMonitor\
+
+$pathStr = "./Reports/DailyReports/"
+$errorOutputPrefix = $pathStr + "device_writer_errors" 
+$dateStr=(Get-Date).ToString("yyyyMMdd")
+$dateSuffix = "." + $dateStr + ".wip.txt"
+$errorOutputFile = $errorOutputPrefix + $dateSuffix 
+$scriptOutputFile = $pathStr + "daily-ctd-output" + $dateSuffix
+
+$null | sc $errorOutputFile
+
+rm $scriptOutputFile
+
+Set-Alias -Name mon_wip -Value .\bin\Debug\net5.0\M2MMonitor.exe
+
+mon_wip putDbUsagesCtd --simStatus=Activated   --NputDb --skipCtdInsert   --errorOutput=$errorOutputFile 1>$scriptOutputFile
+mon_wip putDbUsagesCtd --simStatus=Deactivated --NputDb --skipCtdInsert   --errorOutput=$errorOutputFile 1>>$scriptOutputFile
+mon_wip putDbUsagesCtd --simStatus=Retired     --NputDb --skipCtdInsert   --errorOutput=$errorOutputFile 1>>$scriptOutputFile
+
+#next - baseline in git!!!
 
 popd
